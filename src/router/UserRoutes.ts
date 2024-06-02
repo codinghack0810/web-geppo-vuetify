68ee3f5860c4f391ac29873951397aec8801159d
codinghack0810 <codinghack30@gmail.com>
2024-06-03 00:08:57 +1000

    admin/monthlyreport/detail - initial

--- src/router/UserRoutes.ts
+++ src/router/UserRoutes.ts
@@ -1,42 +1,42 @@
 const UserRoutes = {
-  path: '/user',
-  meta: {
-    requiresAuth: true
-  },
-  redirect: '/user/home',
-  component: () => import('@/layouts/user/UserLayout.vue'),
-  children: [
-    {
-      name: 'Home',
-      path: 'home',
-      component: () => import('@/views/user/home/UserHome.vue')
+    path: '/user',
+    meta: {
+        requiresAuth: true
     },
-    {
-      name: 'WorkSchedule',
-      path: 'workschedule',
-      component: () => import('@/views/user/workschedule/WorkSchedule.vue')
-    },
-    {
-      name: 'MonthlyReport',
-      path: 'monthlyreport',
-      component: () => import('@/views/user/monthlyreport/MonthlyReportDetail.vue')
-    },
-    {
-      name: 'AttendanceList',
-      path: 'attendancelist',
-      component: () => import('@/views/user/attendancelist/AttendanceList.vue')
-    },
-    {
-      name: 'WeeklyReport',
-      path: 'weeklyreport',
-      component: () => import('@/views/user/weeklyreport/WeeklyReport.vue')
-    },
-    {
-      name: 'PersonalSetting',
-      path: 'personalsetting',
-      component: () => import('@/views/user/personalsetting/PersonalSetting.vue')
-    }
-  ]
+    redirect: '/user/home',
+    component: () => import('@/layouts/user/UserLayout.vue'),
+    children: [
+        {
+            name: 'Home',
+            path: 'home',
+            component: () => import('@/views/user/home/UserHome.vue')
+        },
+        {
+            name: 'WorkSchedule',
+            path: 'workschedule',
+            component: () => import('@/views/user/workschedule/WorkSchedule.vue')
+        },
+        {
+            name: 'MonthlyReport',
+            path: 'monthlyreport',
+            component: () => import('@/views/user/monthlyreport/MonthlyReportDetail.vue')
+        },
+        {
+            name: 'AttendanceList',
+            path: 'attendancelist',
+            component: () => import('@/views/user/attendancelist/AttendanceList.vue')
+        },
+        {
+            name: 'WeeklyReport',
+            path: 'weeklyreport',
+            component: () => import('@/views/user/weeklyreport/WeeklyReport.vue')
+        },
+        {
+            name: 'PersonalSetting',
+            path: 'personalsetting',
+            component: () => import('@/views/user/personalsetting/PersonalSetting.vue')
+        }
+    ]
 };
 
 export default UserRoutes;
