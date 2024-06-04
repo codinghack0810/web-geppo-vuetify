c03299c860c6ed50b57e556717d5390e9edf4d89
codinghack0810 <codinghack30@gmail.com>
2024-06-04 18:36:29 +1000

    ts -> js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,19 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier'],
-  env: {
-    'vue/setup-compiler-macros': true
-  },
-  rules: {
-    'comma-dangle': 'off',
-    '@typescript-eslint/comma-dangle': 'off',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'javascript.validate.enable': 0
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        // '@vue/eslint-config-typescript/recommended',
+        '@vue/eslint-config-prettier'
+    ],
+    env: {
+        'vue/setup-compiler-macros': true
+    },
+    rules: {
+        'comma-dangle': 'off',
+        'prettier/prettier': ['error', { endOfLine: 'auto' }]
+        // 'javascript.validate.enable': 0
+    }
 };
